4755d9b9f3ef68ec73af6f354bd48b8915dc1479
Shashuvo <shahariat2810@gmail.com>
2026-09-04 22:56:02 +0600

    picture upload route created

--- src/app/module/auth/auth.route.ts
+++ src/app/module/auth/auth.route.ts
@@ -27,4 +27,5 @@ router.post("/refresh-token", AuthController.refreshToken);
 router.post("/google", AuthController.googleLogin);
 router.post("/forgot-password", validateRequest(UserValidation.ForgotPasswordZodSchema), AuthController.forgotPassword)
 router.post("/reset-password", validateRequest(UserValidation.ResetPasswordZodSchema), AuthController.resetPassword);
+router.post("/upload")
 export const AuthRoutes = router;
