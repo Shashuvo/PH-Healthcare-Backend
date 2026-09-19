0d160abf76a24b12673c7f185d507f7da50603f2
Shashuvo <shahariat2810@gmail.com>
2026-09-19 21:27:17 +0600

    get all doctors added with search and filtering

--- src/app/module/doctor/doctor.route.ts
+++ src/app/module/doctor/doctor.route.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { upload } from "../../lib/multer";
-import { DoctorController } from "./doctor.controller";
 import { Role } from "../../../generated/prisma/enums";
+import { upload } from "../../lib/multer";
 import { auth } from "../../middleware/checkAuth";
-
+import { DoctorController } from "./doctor.controller";
 
 const router = Router();
 
@@ -23,16 +22,18 @@ router.post(
 	]),
 	DoctorController.applyAsDoctor,
 );
-
 router.post(
 	"/apply-as-doctor/verify-email",
 	DoctorController.verifyDoctorEmail,
 );
-
 router.post(
 	"/approve-doctor",
 	auth(Role.ADMIN, Role.SUPER_ADMIN),
 	DoctorController.verifyDoctorEmail,
 );
-
+router.get(
+	"/all-doctors",
+	auth(Role.ADMIN, Role.SUPER_ADMIN),
+	DoctorController.getAllDoctors,
+);
 export const DoctorRoutes = router;
